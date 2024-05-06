21333e375749661da94dd44c415ed6f61a39d7a9
JerryMouseZ <1711333@mail.nankai.edu.cn>
2024-05-06 14:57:03 +0800

    add copilot

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    opts = {
+      debug = false, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+}
