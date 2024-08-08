b4e121e51866a2a8ccff0ac6e3e57e23fa8c0265
JerryMouseZ <1711333@mail.nankai.edu.cn>
2024-08-08 09:53:25 +0800

    dismiss copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -11,4 +11,16 @@ return {
     },
     -- See Commands section for default commands if you want to lazy load on them
   },
+  {
+    "github/copilot.lua",
+    branch = "master",
+    -- See Configuration section for rest
+    -- See Commands section for default commands if you want to lazy load on them
+    opts = {
+      suggestion = {
+        enabled = false,
+        -- See Configuration section for rest
+      },
+    }
+  }
 }
