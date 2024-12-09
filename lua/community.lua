55bed99cf8b0c3f30b656df5e2cedb08d97bcf3b
liz <nku_liz@icloud.com>
2024-12-09 21:31:45 +0800

    chore(community.lua): remove copilot-lua-cmp import

--- lua/community.lua
+++ lua/community.lua
@@ -8,7 +8,7 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.pack.cpp" },
   -- import/override with your plugins folder
 }
